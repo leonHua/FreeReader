4692608a0b3139699ff952d1620661aba99db0de
leon <open_leon@163.com>
2017-03-13 16:30:15 +0800

    优化界面布局

--- fBReader/src/main/java/com/leon/myreader/activity/MainActivity.java
+++ fBReader/src/main/java/com/leon/myreader/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.leon.myreader.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -14,6 +15,7 @@ import org.geometerplus.android.fbreader.libraryService.SQLiteBooksDatabase;
 import org.geometerplus.fbreader.book.Book;
 import org.geometerplus.zlibrary.ui.android.R;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,11 +44,12 @@ public class MainActivity extends Activity {
     }
 
     private void initData() {
-        mBookLists.add("/storage/emulated/0/Download/回到明朝当王爷 校对版.txt");
+        //mBookLists.add("/storage/emulated/0/Download/回到明朝当王爷 校对版.txt");
+        mBookLists.add(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + File.separator + "666.txt");
     }
 
     public void openBook(String path) {
-        //Strin;g path = "/storage/emulated/0/Download/回到明朝当王爷 校对版.txt";
+        //String path = "/storage/emulated/0/Download/回到明朝当王爷 校对版.txt";
         //String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) + File.separator + "666.txt";
         Book book = new Book(1, path, "王爷", null, null);
         FBReader.openBookActivity(this, book, null);
